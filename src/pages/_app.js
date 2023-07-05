2ff16e9b4222648eaf8cff34a43197b875b46052
김진건 <87012465+jingeon27@users.noreply.github.com>
2023-07-05 11:40:00 +0900

    수정

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,14 @@
-import '@/styles/globals.css'
-
+import { Inter } from "next/font/google";
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const inter = Inter({
+    weight: ["400", "500", "600", "700"],
+    subsets: ["latin"],
+  });
+  return (
+    <>
+      <main className={inter.className}>
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
 }
